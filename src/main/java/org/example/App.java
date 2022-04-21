29440a5e4b0d09e432b36e862603360650b28e6c
luca-capotombolo <capoluca98@gmail.com>
2022-04-21 11:38:49 +0200

    Ho inserito la variabile intera commit1

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
+        int commit1 = 1;
+
+        System.out.println(commit1);
         System.out.println( "Hello World!" );
     }
 }
